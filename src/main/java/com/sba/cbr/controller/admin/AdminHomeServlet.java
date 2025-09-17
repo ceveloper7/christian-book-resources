1ead11fe1cc2bd7ecc13ceb67028a93c3661b210
Carlos V. <cevabarcvilla@gmail.com>
2025-09-17 20:58:03 -0300

    System Business Applications - Christian Books Resources - CBR - Admin Home Page

---
+++ src/main/java/com/sba/cbr/controller/admin/AdminHomeServlet.java
@@ -0,0 +1,25 @@
+package com.sba.cbr.controller.admin;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/admin/")
+public class AdminHomeServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public AdminHomeServlet() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String homePage = "index.jsp";
+		RequestDispatcher dispatcher = request.getRequestDispatcher(homePage);
+		dispatcher.forward(request, response);
+	}
+
+}
---
+++ src/main/webapp/admin/footer.jsp
@@ -0,0 +1,4 @@
+<div align="center">
+	<h4>Bookstore Administration</h4>
+	<h4>Copyright (C) 2025 by System Business Applications (SBA) Co, Ltd</h1>
+</div>
