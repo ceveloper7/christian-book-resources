cea47d467bdb935fe8a29b5281e1d55f42f38f42
Carlos V. <cevabarcvilla@gmail.com>
2025-10-29 00:28:18 -0300

    Christian Books Resources - Record objects

--- src/main/java/com/sba/cbr/dataobjects/RUser.java
+++ src/main/java/com/sba/cbr/dataobjects/RUser.java
@@ -3,14 +3,24 @@ package com.sba.cbr.dataobjects;
 import java.time.LocalDateTime;
 import java.util.Objects;
 
-import com.sba.cbr.entity.AuditFields;
-
-public record RUser(Integer id, String email, String password, String fullname, AuditFields audit, boolean active) {
+public record RUser(
+		Integer id, 
+		String email, 
+		String password, 
+		String fullname, 
+		Integer createdBy, 
+		LocalDateTime createdAt, 
+		Integer updatedBy,  
+		LocalDateTime updatedAt,
+		boolean active) {
 	public RUser {
-		Objects.requireNonNull(email, "User email can not be null");
-		Objects.requireNonNull(password, "User passsword can not be null");
-		Objects.requireNonNull(fullname, "User fullname can not be null");
-		Objects.requireNonNull(audit, "User audits fields can not be null");
-		Objects.requireNonNull(active, "User active can not be null");
+		Objects.requireNonNull(email, "User email field can not be null");
+		Objects.requireNonNull(password, "User passsword field can not be null");
+		Objects.requireNonNull(fullname, "User fullname field can not be null");
+		Objects.requireNonNull(createdBy, "User createdBy field fields can not be null");
+		Objects.requireNonNull(createdAt, "User createdAt field can not be null");
+		Objects.requireNonNull(updatedBy, "User updatedBy field can not be null");
+		Objects.requireNonNull(updatedAt, "User updatedAt field can not be null");
+		Objects.requireNonNull(active, "User active field can not be null");
 	}
 }
